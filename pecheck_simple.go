cc0aa9508b6f2afb59a98acd0d8fc9322811e5e5
David Chin <david.w.h.chin@gmail.com>
2018-01-10 23:34:31 -0500

    Deal with "fixedNN" PEs

--- pecheck_simple.go
+++ pecheck_simple.go
@@ -6,6 +6,7 @@ package main
 
 import (
     "strings"
+    "regexp"
     "github.com/dgruber/jsv"
 )
 
@@ -33,12 +34,22 @@ func job_verification_function() {
          * XXX note that this does not properly deal with a range of number of slots;
          *     it just takes the max value of the range 
          */
-        if (strings.EqualFold("shm", pe_name) || strings.EqualFold("matlab", pe_name)) {
+       if (strings.EqualFold("shm", pe_name) || strings.EqualFold("matlab", pe_name)) {
             pe_max, _ := jsv.GetParam("pe_max")
             jsv.SetParam("binding_strategy", "linear_automatic")
             jsv.SetParam("binding_type", "set")
             jsv.SetParam("binding_amount", pe_max)
             jsv.SetParam("binding_exp_n", "0")
+            modified_p = true
+        } else if (strings.HasPrefix(pe_name, "fixed")) {
+            // deal with "fixedNN" PEs
+            pat, _ := regexp.Compile(`^fixed(\d+)`)
+            res := pat.FindStringSubmatch(pe_name)
+            // fmt.Printf("%s\n", res[1]) 
+            jsv.SetParam("binding_strategy", "striding_automatic")
+            jsv.SetParam("binding_type", "set")
+            jsv.SetParam("binding_amount", res[1])
+            jsv.SetParam("binding_step", "1")
             modified_p = true
         }
     }
