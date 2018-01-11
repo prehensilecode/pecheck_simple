62378d7e8500765d23ca3cd146be489f3debe527
David Chin <david.w.h.chin@gmail.com>
2018-01-10 20:58:08 -0500

    Deal with "matlab" pe as well

--- pecheck_simple.go
+++ pecheck_simple.go
@@ -27,11 +27,13 @@ func job_verification_function() {
     } else {
         pe_name, _ := jsv.GetParam("pe_name")
 
-        // XXX the "shm" PE is the single-node multicore PE
-        //     change this to the equivalent for your site
-        // XXX note that this does not properly deal with a range of number of slots;
-        //     it just takes the max value of the range
-        if strings.EqualFold("shm", pe_name) {
+        /* XXX the "shm" PE is the single-node multicore PE
+         *     change this to the equivalent for your site; 
+         *     the "matlab" PE is identically defined to the "shm" PE
+         * XXX note that this does not properly deal with a range of number of slots;
+         *     it just takes the max value of the range 
+         */
+        if (strings.EqualFold("shm", pe_name) || strings.EqualFold("matlab", pe_name)) {
             pe_max, _ := jsv.GetParam("pe_max")
             jsv.SetParam("binding_strategy", "linear_automatic")
             jsv.SetParam("binding_type", "set")
